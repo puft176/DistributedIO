c959c067ec4c628cb5d812c9e1829464c4ba21ea
puft <1761475595@qq.com>
2024-05-09 16:43:11 +0800

    添加三块核心板

---
+++ IOOutput_board/User/main.c
@@ -0,0 +1,13 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+
+int main(void)
+{
+	/*模块初始化*/
+	OLED_Init();		//OLED初始化
+	while (1)
+	{
+		
+	}
+}
